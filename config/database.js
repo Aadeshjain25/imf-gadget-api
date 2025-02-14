409376027b4becae44a1385a535ba013a93a5417
Aadesh Jain <aadeshjain2004@gmail.com>
2025-02-15 02:43:30 +0530

    Fixed DATABASE_URL issue

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set in environment variables!");
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false, // Set to true if you want to see SQL queries in logs
